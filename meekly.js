2fcd5cc879f4addcbfa3e22675bee6f5f2d14528
raw-bit <kljeffery@gmail.com>
2017-12-02 09:14:35 +0000

    Moving keys to env

--- meekly.js
+++ meekly.js
@@ -1,19 +1,31 @@
 const Twitter = require('twitter');
 const request = require('request');
 const schedule = require('node-schedule')
+const env = require('node-env-file')
 const fs = require('fs');
+
+env(__dirname + '/.env')
+
+let CONSUMER_KEY = process.env.TwitterConsumerKey
+let CONSUMER_SECRET = process.env.TwitterConsumerSecret
+let ACCESS_TOKEN_KEY = process.env.TwitterAccessKey
+let ACCESS_TOKEN_SECRET = process.env.TwitterAccessSecret
+let USERNAME = process.env.LastfmUser
+let LASTFMAPIKEY = process.envLastfmKey
+
 const options = {
-    url: 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=USERNAME&api_key=YOURAPIKEY&format=json&period=7day&limit=3',
+    url: 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=' + USERNAME + '&api_key=' + LASTFMAPIKEY + '&format=json&period=7day&limit=3',
     headers: {
         'User-Agent': 'Meekly'
     }
-};
+}
+
 
 const client = new Twitter({
-    consumer_key: 'CONSUMER KEY',
-    consumer_secret: 'CONSUMER SECRET',
-    access_token_key: 'ACCESS TOKEN KEY',
-    access_token_secret: 'ACCESS TOKEN SECRET'
+    consumer_key: 'CONSUMER_KEY',
+    consumer_secret: 'CONSUMER_SECRET',
+    access_token_key: 'ACCESS_TOKEN_KEY',
+    access_token_secret: 'ACCESS_TOKEN_SECRET'
 });
 
 var artists = 'My top artists this week: ';
